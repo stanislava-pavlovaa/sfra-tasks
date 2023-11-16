aab794427f947d786ea061d92bb60a93097ae3fb
stanislava-pavlovaa <stanislava.pavlova@c4nexus.com>
2023-11-16 15:05:18 +0200

    Refactored product controller

--- cartridges/app_custom_slots/cartridge/controllers/Product.js
+++ cartridges/app_custom_slots/cartridge/controllers/Product.js
@@ -4,24 +4,12 @@ var server = require("server");
 server.extend(module.superModule);
 
 server.append("Show", function (req, res, next) {
-    var Resource = require('dw/web/Resource');
-    var ContentMgr = require("dw/content/ContentMgr");
-    var asset = ContentMgr.getContent("product-information");
-
-    if (asset && asset.custom.body) {
-        var viewData = res.getViewData();
-        var assetBody = asset.custom.body.toString();
-
-        if (viewData.product.shipping) {
-            var shippingInfo = viewData.product.shipping;
-            assetBody = assetBody.replace("{shipping}", shippingInfo);
-        } else {
-            assetBody = assetBody.replace("{shipping}", Resource.msg('shipping.no.information', 'product', null));
-        }
-
-        viewData.assetBody = assetBody;
-        res.setViewData(viewData);
-    }
+    var viewData = res.getViewData();
+    
+    var shippingInfo = viewData.product.shippingInformation;
+    viewData.shippingInfo = shippingInfo;
+    
+    res.setViewData(viewData);
     next();
 });
 
--- cartridges/app_custom_slots/cartridge/models/product/decorators/shipping.js
+++ cartridges/app_custom_slots/cartridge/models/product/decorators/shipping.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
+
+var Resource = require("dw/web/Resource");
+var ContentMgr = require("dw/content/ContentMgr");
 
 module.exports = function (object, product) {
-    Object.defineProperty(object, 'shipping', {
+    Object.defineProperty(object, "shipping", {
         enumerable: true,
-        value: product.custom.shipping ? product.custom.shipping : null
+        value: product.custom.shipping ? product.custom.shipping : null,
+    });
+    Object.defineProperty(object, "shippingInformation", {
+        enumerable: true,
+        value: (function() {
+            var asset = ContentMgr.getContent("product-information");
+
+            if (asset && asset.custom.body && asset.online) {
+                var assetBody = asset.custom.body.toString();
+                if (product.custom.shipping) {
+                    var shippingInfo = product.custom.shipping;
+                    assetBody = assetBody.replace("{shipping}", shippingInfo);
+                } else {
+                    assetBody = assetBody.replace("{shipping}", Resource.msg("shipping.no.information", "product", null));
+                }
+                return assetBody;
+            }
+        }())
     });
 };
